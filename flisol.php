7e787e02252c76a83f3a0dd34e67e92aca6a0a6d
Oliver Etchebarne <yo@drmad.org>
2018-04-27 22:34:50 -0500

    Página del FLISoL

---
+++ flisol.php
@@ -0,0 +1,41 @@
+<?php include 'inc/head.php' ?>
+<body>
+<?php include 'inc/header.php' ?>
+
+<div class="ed-container">
+<div class="flisol">
+
+<img src="static/img/flisol.png" alt="FLISoL" />
+
+<p>Cuándo: <strong>28 de abril del 2018</strong><br />
+Dónde: <strong>Facultad de Ing. de Sistemas de la Universidad Nacional "San Luis Gonzaga" de Ica</strong></p>
+
+<p><img src="static/img/invitacion-flisol-2018.jpg" /></p>
+
+<div><a href="static/invitacion-flisol-2018.tar.xz">Descarga aquí la invitación</a></div>
+<div><a href="static/invitacion-flisol-2018.tar.xz">invitacion-flisol-2018.tar.xz</a></div>
+<div>SHA1 0e928980d18904e896c052e5a28c4e8bf4bb0f50</div>
+
+<p>Requisitos: Linux 64 bits con las librerías <code>libmikmod</code>, <code>libsdl2</code> y <code>libsdl2_image</code>.</p>
+
+<p>En <a href="https://ubuntu.com/">Ubuntu Linux</a> puedes instalarlas ejecutando:</p>
+
+<pre>
+sudo apt -y install libmikmod3 libsdl2-2.0-0 libsdl2-image-2.0-0
+</pre>
+
+<p>En <a href="https://manjaro.org/">Manjaro Linux</a> debes <a href="https://wiki.manjaro.org/index.php/Arch_User_Repository">activar los repositorios AUR</a>, y ejecutar:</p>
+
+<pre>
+sudo pacman -S sdl2 sdl2_image libmikmod
+</pre>
+
+<p>¡Déjanos tus comentarios en <a href="https://www.facebook.com/icanux/">nuestro fanpage en Facebook</a>!</p>
+
+<p>Tambíen puedes ver un vídeo de la invitación <a href="https://youtu.be/F6-1KqnQfcY" target="_blank">aquí</a> (aunque no lo recomendamos, está en baja calidad)</p>
+<h1>Mapa</h1>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3869.814009449563!2d-75.73353970135334!3d-14.088154074329529!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9110e28a886bb93b%3A0x8a1345519fcf2cbc!2sFacultad+de+Ingenier%C3%ADa+de+Sistemas!5e0!3m2!1ses-419!2spe!4v1524107434826" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+</body>
+</html>
